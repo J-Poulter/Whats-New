7a955ed6d852019c9c02faf239449eb373db06b1
Jeremy Poulter <Jpoulter@gmail.com>
2020-03-17 23:01:57 -0600

    Add html elements to the components return and implementt props to appropriate locations

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import './NewsArticle.css';
 
-// NEWSARTICLE COMPONENT CODE GOES HERE
+const NewsArticle = (props) => {
+
+  return (
+    <article>
+      <div className='articleCard' id={props.id}>
+        <img>{props.img}</img>
+      </div>
+      <div>
+        <h3>{props.headline}</h3>
+        <p>{props.description}</p>
+      </div>
+      <div>
+        <a href={props.url}>Link To Article</a>
+        <icon></icon>
+      </div>
+    </article>
+  )
+}
 
 export default NewsArticle;
