96876dea6007943e771dcd6f27949aac6c9ac1f5
Jeremy Poulter <Jpoulter@gmail.com>
2020-03-17 23:02:28 -0600

    Flesh out NewsContainer functional component

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import './NewsContainer.css'
+import NewsArticle from '../NewsArticle/NewsArticle';
 
-// NEWSCONTAINER COMPONENT CODE GOES HERE
+const NewsContainer = (props) => {
+
+  return (
+    <section> 
+      {
+        props.local.map(article => (
+          <NewsArticle 
+            id={article.id}
+            img={article.img}
+            headling={article.headline}
+            description={article.description}
+            url={article.url}
+          />
+        ))
+      }
+    </section>
+  )
+}
 
 export default NewsContainer;
