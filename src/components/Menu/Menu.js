55fd069b2ae30af5769edbf0ef2566e1010e18eb
Jeremy Poulter <Jpoulter@gmail.com>
2020-03-17 20:05:21 -0600

    Set up functional component structure for menu bar

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -2,5 +2,16 @@ import React from 'react';
 import './Menu.css'
 
 // MENU COMPONENT CODE GOES HERE
+ const Menu = (props) => {
+    let titles = props.categories.map(cate => {
+        return <p>{cate}</p>
+    })
+    return (
+        <div>
+            {titles}
+            <p>{props.newThing}</p>
+        </div>
+    )
+}
 
 export default Menu;
