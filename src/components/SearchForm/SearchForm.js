adb56d48629412f4ffaefe0c69d93afdb3f8e628
Jeremy Poulter <Jpoulter@gmail.com>
2020-03-17 20:06:22 -0600

    Set up a general functional component structure for the search form

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,5 +2,13 @@ import React from 'react';
 import './SearchForm.css';
 
 // SEARCHFORM COMPONENT CODE GOES HERE
-
+ const SearchForm = () => {
+    
+    return (
+        <form>
+            <input placeholder='Search Text Here'/>
+            <button>Click To Search</button>
+        </form>
+    )
+}
 export default SearchForm;
