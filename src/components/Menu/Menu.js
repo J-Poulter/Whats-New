a8edd050a680442c2c85e47845178feea378c5f1
Jeremy Poulter <Jpoulter@gmail.com>
2020-03-17 23:00:56 -0600

    Add html elements that will be used in the functions return

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import './Menu.css'
 
-// MENU COMPONENT CODE GOES HERE
  const Menu = (props) => {
-    let titles = props.categories.map(cate => {
-        return <p>{cate}</p>
-    })
-    return (
-        <div>
-            {titles}
-            <p>{props.newThing}</p>
-        </div>
-    )
+  /*let titles = props.categories.map(cate => {
+      return <p>{cate}</p>
+  })*/
+  return (
+    <div>
+      <h1>What's New?</h1>
+      <div>
+        <icon></icon>
+        <p>Local News</p>
+      </div>
+      <div>
+        <icon></icon>
+        <p>Technology</p>
+      </div>
+      <div>
+        <icon></icon>
+        <p>Entertainment</p>
+      </div>
+      <div>
+        <icon></icon>
+        <p>Science</p>
+      </div>
+      <div>
+        <icon></icon>
+        <p>Health</p>
+      </div>
+    </div>
+  )
 }
 
 export default Menu;
