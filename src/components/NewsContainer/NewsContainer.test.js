add14ac40a1a2b6ba72a551232acebb0d1a59817
Jeremy Poulter <Jpoulter@gmail.com>
2020-03-19 16:54:44 -0600

    Create test to check if render matches the snapshot and if the correct articles are being displayed

--- src/components/NewsContainer/NewsContainer.test.js
+++ src/components/NewsContainer/NewsContainer.test.js
@@ -6,7 +6,15 @@ import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 describe('NewsContainer', () => {
-  it('should', () => {
-
+  it('should render an element structure that matches the snapshot', () => {
+    let mockCategories = [
+      { title: 'entertainment', dataset: 'entertainment', iconImg: '★' },
+      { title: 'local', dataset: 'local', iconImg: '💡' }
+    ];
+    const snap = render(<NewsContainer 
+      category={mockCategories}
+    />)
+    expect(snap).toMatchSnapshot();
   });
+
 });
