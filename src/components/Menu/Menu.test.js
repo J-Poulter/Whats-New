e39ed2144bd4398ddb3a2b996cdb9d0de5f59b10
Jeremy Poulter <Jpoulter@gmail.com>
2020-03-19 16:53:08 -0600

    Create tests to check for menu snapshot and if it invokes the method from parent element with correct arguments

--- src/components/Menu/Menu.test.js
+++ src/components/Menu/Menu.test.js
@@ -6,7 +6,32 @@ import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 describe('Menu', () => {
-  it('should', () => {
+  it('should render an element structure that matches the snapshot', () => {
+    let mockCategories = [
+      { title: 'entertainment', dataset: 'entertainment', iconImg: '★' },
+      { title: 'local', dataset: 'local', iconImg: '💡' }
+    ];
+    const snap = render(<Menu 
+      categories={mockCategories}
+    />)
+    expect(snap).toMatchSnapshot();
+  });
 
+  it('should change the selected category of new articles display after clicking a button', () => {
+    const mockCategories = [
+      { title: 'entertainment', dataset: 'entertainment', iconImg: '' }, 
+      { title: 'technology', dataset: 'technology', iconImg: '' }
+    ];
+    const mockChangeCategory = jest.fn();
+    
+    const { getByText } = render(<Menu 
+      selectCategory={mockChangeCategory}
+      categories={mockCategories}
+    />);
+
+    const entertainmentButton = getByText('entertainment');
+    fireEvent.click(entertainmentButton)
+
+    expect(mockChangeCategory).toHaveBeenCalledWith('entertainment')
   });
 });
