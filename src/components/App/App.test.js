4eff1fefbd68db5bab663e160f3bf4e455564403
Jeremy Poulter <Jpoulter@gmail.com>
2020-03-19 16:52:05 -0600

    Create tests to check for app snapshot and two methods that alter its state

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -12,7 +12,28 @@ describe('App', () => {
     ReactDOM.unmountComponentAtNode(div);
   });
 
-  it('should render all components needed', () => {
+  it('should render all components needed to match the snapshot', () => {
+    const snap = render(<App />)
+    expect(snap).toMatchSnapshot();
+  });
 
+  it('should change the articles displayed after user clicks on a new category', () => {
+    const { getByText } = render(<App />)
+    const entertainmentButton = getByText('★entertainment')
+
+    fireEvent.click(entertainmentButton)
+
+    expect(getByText('The final season of BoJack Horseman starts October 25th')).toBeInTheDocument();
+  });
+
+  it('should change the articles displayed after user searches for a given headline title', () => {
+    const { getByPlaceholderText, getByText } = render(<App />)
+    const searchInput = getByPlaceholderText('Search for news article here.')
+    const searchButton = getByText('Search Now')
+
+    fireEvent.change(searchInput, { target: { value: 'giant chicken' } })
+    fireEvent.click(searchButton)
+
+    expect(getByText('Giant Chicken Skeleton In Denver’s Central Library Stands For More Than Just Dinner')).toBeInTheDocument();
   });
 });
